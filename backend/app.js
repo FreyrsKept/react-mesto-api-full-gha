7209d5c5b152da441f5fd451bc4e5dbd36418ef2
FreyrsKept <120368406+FreyrsKept@users.noreply.github.com>
2024-01-15 13:29:09 +0700

    add backend

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,29 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const auth = require('./middlewares/auth');
 const { validateLogin, validateCreateUser } = require('./middlewares/validation');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { createUser, login } = require('./controllers/users');
 const NotFound = require('./errors/notFound');
 const { PORT, MONGO_URL, INTERNAL_SERVER_STATUS } = require('./config/config');
 const { userRouter, cardRouter } = require('./routes');
+const cors = require('./middlewares/cors');
 const app = express();
 
 app.use(express.json());
 
 mongoose.connect(MONGO_URL);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.use(requestLogger);
+app.use(cors);
 
 app.post('/signin', validateLogin, login);
 app.post('/signup', validateCreateUser, createUser);
@@ -24,6 +36,7 @@ app.use('*', (req, res, next) => {
   next(new NotFound('Такой страницы не существует'));
 });
 
+app.use(errorLogger);
 app.use(errors());
 
 app.use((err, req, res, next) => {
